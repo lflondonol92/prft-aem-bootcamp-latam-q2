ad86cda934244d2fee2f102655bbc0f316a1af2a
cctorres <c.torresalzate@gmail.com>
2024-05-31 13:17:52 -0500

    AEM Bootcamp-CT006:Implementing isHomePage

--- core/src/main/java/com/aembootcamp/core/models/BreadcrumbModel.java
+++ core/src/main/java/com/aembootcamp/core/models/BreadcrumbModel.java
@@ -2,40 +2,33 @@ package com.aembootcamp.core.models;
 
 import com.adobe.cq.wcm.core.components.models.Breadcrumb;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
 import lombok.experimental.Delegate;
+
+import javax.inject.Inject;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = Breadcrumb.class, resourceType = BreadcrumbModel.RESOURCE_TYPE)
 public class BreadcrumbModel implements Breadcrumb {
 
     protected static final String RESOURCE_TYPE = "aem-bootcamp/components/structure/breadcrumb";
 
-    @SlingObject
-    private Resource currentResource;
+    @Inject
+    private Page currentPage;
 
-    @SlingObject
-    private ResourceResolver resourceResolver;
-
-    @Delegate @Self @Via(type = ResourceSuperType.class)
+    @Delegate
+    @Self
+    @Via(type = ResourceSuperType.class)
     private Breadcrumb breadcrumb;
 
-    public boolean isHomePath() {
-        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-        if (pageManager != null) {
-            Page currentPage = pageManager.getContainingPage(currentResource);
-            if (currentPage != null) {
-                String pagePath = currentPage.getPath();
-                return pagePath != null && pagePath.endsWith("/home");
-            }
+    public boolean isHomePage() {
+        if (currentPage != null) {
+            String templateName = currentPage.getTemplate().getName();
+            return templateName != null && templateName.equals("homepage");
         }
         return false;
     }
