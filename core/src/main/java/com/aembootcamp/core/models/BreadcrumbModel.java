844e22f09c0c595dec9bd330d801532815c9de5a
emelmorafer <emelmorafer@hotmail.com>
2024-05-31 07:55:13 -0500

    This commit make some corrections in the model and in the styles also add lombok

--- core/src/main/java/com/aembootcamp/core/models/BreadcrumbModel.java
+++ core/src/main/java/com/aembootcamp/core/models/BreadcrumbModel.java
@@ -1,15 +1,21 @@
 package com.aembootcamp.core.models;
 
+import com.adobe.cq.wcm.core.components.models.Breadcrumb;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
+
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
+import lombok.experimental.Delegate;
 
-@Model(adaptables = Resource.class, resourceType = BreadcrumbModel.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class BreadcrumbModel {
+@Model(adaptables = SlingHttpServletRequest.class, adapters = Breadcrumb.class, resourceType = BreadcrumbModel.RESOURCE_TYPE)
+public class BreadcrumbModel implements Breadcrumb {
 
     protected static final String RESOURCE_TYPE = "aem-bootcamp/components/structure/breadcrumb";
 
@@ -18,6 +24,9 @@ public class BreadcrumbModel {
 
     @SlingObject
     private ResourceResolver resourceResolver;
+
+    @Delegate @Self @Via(type = ResourceSuperType.class)
+    private Breadcrumb breadcrumb;
 
     public boolean isHomePath() {
         PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
